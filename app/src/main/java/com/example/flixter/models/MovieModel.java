abf6dee5564b30adb07aea1637df2b02da450db4
Rene B. Dena <renedena842@gmail.com>
2021-08-28 12:39:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flixter/models/MovieModel.java
@@ -0,0 +1,89 @@
+package com.example.flixter.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class MovieModel {
+
+    String posterPath;
+    String title;
+    String overview;
+    String backdropPath;
+    double vote_average;
+    int movieId;
+
+    // Empty constructor needed by the Parceler library
+    public MovieModel() {
+
+    }
+
+    public MovieModel(JSONObject jsonObject) throws JSONException {
+        this.posterPath = jsonObject.getString("poster_path");
+        this.title = jsonObject.getString("title");
+        this.overview = jsonObject.getString("overview");
+        this.backdropPath = jsonObject.getString("backdrop_path");
+        this.vote_average = jsonObject.getDouble("vote_average");
+        this.movieId = jsonObject.getInt("id");
+    }
+
+    public static List<MovieModel> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<MovieModel> movies = new ArrayList<>();
+
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new MovieModel(movieJsonArray.getJSONObject(i)));
+        }
+
+        return movies;
+    }
+
+    public double getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(double vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+}
